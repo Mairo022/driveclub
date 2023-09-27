c0b0663c726732d9becce467bb5f6066f4b9eef8
Mairo022 <mairo022@outlook.com>
2023-09-27 08:35:20 +0200

    Add header

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,16 @@
+import {ReactElement} from "react";
+import "./styles/header.scss";
+
+export function Header(): ReactElement {
+    return (
+        <header className="header">
+            <img className="logo"
+                 src="src/assets/images/logo-server.svg"
+                 alt="Server Logo"
+            />
+            <h1 className="title">
+                Driving Club
+            </h1>
+        </header>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import 'normalize.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {Servers} from "./pages/servers/Servers";
 import {TopBar} from "./components/ui/TopBar";
+import {Header} from "./components/ui/Header";
 
 const router = createBrowserRouter([
     {
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <TopBar/>
+        <Header/>
         <RouterProvider router={router}/>
     </React.StrictMode>,
 )
