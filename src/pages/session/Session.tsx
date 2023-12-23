b2ee4b4e2ed437ecc00d3225506b3083d384d7a8
Mairo022 <mairo022@outlook.com>
2023-12-23 17:09:21 +0200

    Create layout and styling for Session

--- src/pages/session/Session.tsx
+++ src/pages/session/Session.tsx
@@ -2,6 +2,7 @@ import {ReactElement, useEffect, useState} from "react";
 import {FETCH_STATUS} from "../../data/constants";
 import {Table} from "../../components/table/Table";
 import {DriverLaps} from "./DriverLaps";
+import "./style/session.scss";
 
 export function Session(): ReactElement {
     const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -19,26 +20,45 @@ export function Session(): ReactElement {
         <article className="session">
             {isSuccess && <>
                 <div className="details">
-                    <header>
-                        <h4>Track</h4>
-                        <p>Date</p>
+                    <header className="details__header">
+                        <h3 className="details__header__track">Spa (Qualify), 10 minutes</h3>
+                        <p className="details__header__date">10 December 2023 05:11</p>
                     </header>
-                    <div>
-                        <p>Air temp</p>
-                        <p>Road temp</p>
-                        <p>Tyre wear</p>
-                        <p>Fuel rate</p>
-                    </div>
-                    <div>
-                        <p>Type</p>
-                        <p>Duration</p>
-                        <p>Damage</p>
-                        <p>Penalties</p>
+                    <div className="details__conditions">
+                        <div className="conditions">
+                            <div className="conditions__row">
+                                <p className="conditions__row__header">Air:</p>
+                                <p className="conditions__row__data">30c</p>
+                            </div>
+                            <div className="conditions__row">
+                                <p className="conditions__row__header">Road:</p>
+                                <p className="conditions__row__data">5c</p>
+                            </div>
+                        </div>
+                        <div className="conditions">
+                            <div className="conditions__row">
+                                <p className="conditions__row__header">Tyre wear:</p>
+                                <p className="conditions__row__data">1.0</p>
+                            </div>
+                            <div className="conditions__row">
+                                <p className="conditions__row__header">Fuel rate:</p>
+                                <p className="conditions__row__data">1.0</p>
+                            </div>
+                        </div>
+                        <div className="conditions">
+                            <div className="conditions__row">
+                                <p className="conditions__row__header">Damage:</p>
+                                <p className="conditions__row__data">100%</p>
+                            </div>
+                            <div className="conditions__row">
+                                <p className="conditions__row__header">Penalties:</p>
+                                <p className="conditions__row__data">On</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="drivers">
                     <div className="driver">
-                        <h4>Driver</h4>
                         {/* Into Table: car, time, fastest lap, gap to first, rank */}
                         <Table data={[{}]}/>
                         <button>Dropdown</button>
